a208865c0eebb141502c7c85d9083c16898d327c
AlexCondur <alexcondur95@gmail.com>
2022-05-19 22:23:48 +0300

    utilitary functions for nodes

--- src/components/FlowDropout/FlowDropout.tsx
+++ src/components/FlowDropout/FlowDropout.tsx
@@ -1,30 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 
 import { mdiCogOutline } from "@mdi/js";
 
 import { useGetDropoutData } from "../../hooks/useGetDropoutData";
 
+import { convertNodesObjectToArray, generateBranches } from "../../utils/nodes";
+
 import Button, { IconButton } from "../Button";
 import BarChart, { BarType, BarData } from "../BarChart";
 
-import { NodeType } from "../../types/Node";
+import { NodeType, NodeElement } from "../../types/Node";
 
 import { Container, Header, HeaderActions, Body } from "./styled";
 
 const FlowDropout = () => {
-  const [branch, setBranch] = useState();
+  const [activeBranch, setActiveBranch] = useState<string[]>([]);
   const { isLoading, data } = useGetDropoutData();
 
-  console.log(isLoading, data);
-  const parsedData =
-    data &&
-    Object.keys(data).map((key) => ({
-      id: key,
-      ...data[key],
-    }));
-
+  const nodeList = useMemo(() => convertNodesObjectToArray(data), [data]);
+  const branches = useMemo(() => generateBranches(nodeList), [nodeList]);
+  const nodesBasedOnBranch = data
+    ? activeBranch.map(
+        (nodeId: string): NodeElement => ({
+          id: nodeId,
+          ...data[nodeId],
+        })
+      )
+    : [];
   const barchartData =
-    parsedData?.map(
+    nodesBasedOnBranch?.map(
       (node): BarData => ({
         id: node.id,
         label: node.label,
@@ -33,6 +37,12 @@ const FlowDropout = () => {
           node.type === NodeType.BASIC ? BarType.PRIMARY : BarType.SECONDARY,
       })
     ) || [];
+
+  useEffect(() => {
+    if (branches.length > 0) {
+      setActiveBranch(branches[0]);
+    }
+  }, [branches]);
 
   return (
     <Container>
--- src/types/Node.ts
+++ src/types/Node.ts
@@ -13,3 +13,9 @@ export interface Node {
 export interface NodesObject {
   [nodeId: string]: Node;
 }
+
+export interface NodeElement extends Node {
+  id: string;
+}
+
+export type NodeList = NodeElement[];
